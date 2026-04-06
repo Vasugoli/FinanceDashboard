88bd22df652f4af1164e8f45a43a24dbf5ff7938
Vasugoli <golivasu7@gmail.com>
2026-04-06 16:41:31 +0530

    feat: add GitHub Actions workflow for deployment and create 404 and index HTML pages

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,11 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 const config = defineConfig({
+  base: '/FinanceDashboard/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
